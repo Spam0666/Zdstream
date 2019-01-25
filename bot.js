a09112e228c467e870d23358f00aadb5ced65704
Spam0666 <46114545+Spam0666@users.noreply.github.com>
2019-01-25 12:47:24 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,11 @@
 const Discord = require("discord.js")
 const client = new Discord.Client();
-var prefix = "A"
+var prefix = "z"
 
 
 
 client.on('message', function(message) {
-    const myID = "462038057529507841";
+    const myID = "515227572385218570";
     let args = message.content.split(" ").slice(1).join(" ");
     if(message.content.startsWith(prefix + "sn")) {
                 if(message.author.id !== myID) return;
